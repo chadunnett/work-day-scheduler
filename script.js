be9ea5733f046910876a30248081fc213236f39e
chadunnett <chadunnett@gmail.com>
2022-10-03 18:31:32 -0500

    fixes

--- script.js
+++ script.js
@@ -12,9 +12,15 @@ function timeOfDay() {
         var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
         if (timeBlock < currentTime) {
             $(this).addClass("past");
+            $(this).removeClass("present")
+            $(this).removeClass("future")
         } else if (timeBlock === currentTime) {
+            $(this).removeClass("past")
            $(this).addClass("present");
+           $(this).removeClass("future")
         } else {
+            $(this).removeClass("past")
+            $(this).removeClass("present")
             $(this).addClass("future")
         }
     })
