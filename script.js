d0e15fdf58afa8784b2ed2f4abc5ab5e64c1c000
chadunnett <chadunnett@gmail.com>
2022-10-03 13:37:12 -0500

    class fix

--- script.js
+++ script.js
@@ -12,15 +12,9 @@ function timeOfDay() {
         var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
         if (timeBlock < currentTime) {
             $(this).addClass("past");
-            // $(this).removeClass("present")
-            // $(this).removeClass("future");
         } else if (timeBlock === currentTime) {
-            // $(this).removeClass("past");
-            $(this).addClass("present");
-            // $(this).removeClass("furure")
+           $(this).addClass("present");
         } else {
-            // $(this).removeClass("past");
-            // $(this).removeClass("present");
             $(this).addClass("future")
         }
     })
